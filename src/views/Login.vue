52ea6993d3980e646c5389c868152da919231f4b
Jose Jaime Come <josejaimecome@gmail.com>
2023-04-09 14:53:25 +0200

    Button and Label component added

---
+++ src/views/Login.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Button from '../components/Button.vue'
+import Label from '../components/Label.vue'
+</script>
+
+<template>
+  <main>
+    <Button text="Button" />
+    <Label text="Label" />
+  </main>
+</template>
