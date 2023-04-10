3ca3ee22ec39d883f524ef8c2af86157726f4edf
Jose Jaime Come <josejaimecome@gmail.com>
2023-04-10 22:06:40 +0200

    Login Form added

---
+++ src/components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+    <footer>
+      <p>Copyright &copy; 2023</p>
+      <router-link to="/about">About</router-link>
+    </footer>
+</template>
+  
+<style scoped>
+a {
+   color: #333;
+}
+  
+footer {
+  margin: auto;
+  width: 50%;  
+  margin-top: 30px;
+  text-align: center;
+}
+</style>
